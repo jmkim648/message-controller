298ddddb49db889226b529888e6f25dfb40fddd3
Jaemin Kim <jmkim648@gmail.com>
2023-09-11 17:39:29 +0900

    Update main.py
    코드 정리

--- main.py
+++ main.py
@@ -9,36 +9,34 @@ import threading
 
 scheduler = schedule.Scheduler()
 
-if __name__ == '__main__':
+def exit_app(icon, item):
+    # 필요하다면 종료 전 데이터 정리
+    scheduler.clear()
+    icon.stop()
+    sys.exit(0)
+
+def run_schedule():
+    while True:
+        scheduler.run_pending()
+        time.sleep(60)  
+
+def main():
     image = Image.open("./img/favicon.ico")
+    scheduler.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
 
-    def exit_app(icon, item):
-        # 필요하다면 종료 전 데이터 정리
-        scheduler.clear()
-        icon.stop()
-        sys.exit(0)
+    menu = (
+        pystray.MenuItem('Play ladder', play_ladder),
+        pystray.MenuItem('Exit', exit_app)
+    )
+    icon = pystray.Icon("잔디봇", image, "잔디봇", menu)
 
-    def run_schedule():
-        while True:
-            scheduler.run_pending()
-            time.sleep(60)  
+    schedule_thread = threading.Thread(target=run_schedule)
+    schedule_thread.daemon = True
+    schedule_thread.start()
 
-    def main():
-        scheduler.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
+    icon.run()
 
-        menu = (
-            pystray.MenuItem('Play ladder', play_ladder),
-            pystray.MenuItem('Exit', exit_app)
-        )
-        icon = pystray.Icon("잔디봇", image, "잔디봇", menu)
-
-        schedule_thread = threading.Thread(target=run_schedule)
-        schedule_thread.daemon = True
-        schedule_thread.start()
-
-        icon.run()
-
-    
+if __name__ == '__main__':    
     main()
     
 
