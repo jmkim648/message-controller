9914960bbe1f272af91a8e8db1159f10ada5dbd9
Jaemin Kim <jmkim648@gmail.com>
2023-09-12 00:39:48 +0900

    ---다운그레이드-----
    문제: 스케쥴러와 pystray 기능을 같이 쓰면 스케쥴러 진입은 하되, 내부에서 돌다가 윈도우 핸들을 받아올 때 에러 출력
     - 관리자권한으로 main.py 실행해도 실패
     pystray로 만든 아이콘이 윈도우 핸들을 다루는 권한이 없는게 아닌가 추정중이지만 현재는 해결 불가

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 import time
-import schedule
-import pystray
-import sys
-from PIL import Image
 from check_github import *
 from play_ladder import *
-import threading
+import schedule
+# import pystray
+# import sys
+# from PIL import Image
+# import threading
 
-scheduler = schedule.Scheduler()
+# scheduler = schedule.Scheduler()
 
-def exit_app(icon, item):
-    # 필요하다면 종료 전 데이터 정리
-    scheduler.clear()
-    icon.stop()
-    sys.exit(0)
+# def exit_app(icon, item):
+#     # 필요하다면 종료 전 데이터 정리
+#     scheduler.clear()
+#     icon.stop()
+#     sys.exit(0)
 
-def run_schedule():
-    while True:
-        scheduler.run_pending()
-        time.sleep(60)  
+# def run_schedule():
+#     while True:
+#         scheduler.run_pending()
+#         time.sleep(60)  
 
 def main():
-    image = Image.open("./img/favicon.ico")
-    scheduler.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
+    # image = Image.open("./img/favicon.ico")
+    schedule.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
+    # scheduler.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
+    # scheduler.every(30).seconds.do(call_people_to_plant) # from check_github.py
 
-    menu = (
-        pystray.MenuItem('Play ladder', play_ladder),
-        pystray.MenuItem('Exit', exit_app)
-    )
-    icon = pystray.Icon("잔디봇", image, "잔디봇", menu)
+    # menu = (
+    #     pystray.MenuItem('Play ladder', play_ladder),
+    #     pystray.MenuItem('Exit', exit_app)
+    # )
+    # icon = pystray.Icon("잔디봇", image, "잔디봇", menu)
 
-    schedule_thread = threading.Thread(target=run_schedule)
-    schedule_thread.daemon = True
-    schedule_thread.start()
+    # schedule_thread = threading.Thread(target=run_schedule)
+    # schedule_thread.daemon = True
+    # schedule_thread.start()
 
-    icon.run()
+    # icon.run()
+    while True:
+        # scheduler.run_pending()
+        schedule.run_pending()
+        time.sleep(60) 
 
 if __name__ == '__main__':    
     main()
