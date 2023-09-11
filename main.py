ed042b519b45823a0f3248d4b6d73ee42a5b36c0
Jaemin Kim <jmkim648@gmail.com>
2023-09-11 17:05:11 +0900

    fix main.py
    [수정내용]
    종료 시 스케쥴러가 백그라운드에 남아있었기 때문에 clear해주는 부분 추가
    
    아이콘과 스케쥴러에서 각각 while True가 돌아가야하므로 쓰레드 부분 추가
    
    테스트 후 이슈 닫을 것

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from PIL import Image
 from check_github import *
 from play_ladder import *
 
+scheduler = schedule.Scheduler()
+
 if __name__ == '__main__':
     image = Image.open("./img/favicon.ico")
 
     def exit_app(icon, item):
         # 필요하다면 종료 전 데이터 정리
+        scheduler.clear()
         icon.stop()
         sys.exit(0)
 
     def main():
-        schedule.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
+        scheduler.every().day.at("22:00").do(call_people_to_plant) # from check_github.py
 
         menu = (
             pystray.MenuItem('Play ladder', play_ladder),
@@ -25,7 +28,7 @@ if __name__ == '__main__':
         icon.run()
 
         while True:
-            schedule.run_pending()
+            scheduler.run_pending()
             time.sleep(60)    
     
     main()
